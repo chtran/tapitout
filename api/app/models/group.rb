edc8b6057a96829172c72816a4caa8e3fb0898d9
Austin Gulati <contact@austingulati.com>
2013-01-12 13:51:44 -0500

    Remove api/

--- api/app/models/group.rb
+++
@@ -1,4 +0,0 @@
-class Group < ActiveRecord::Base
-  attr_accessible :owner_id
-  belongs_to :owner, class_name: "User"
-end
--- api/spec/factories/groups.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :group do
-    owner_id 1
-  end
-end
