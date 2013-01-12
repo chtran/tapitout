8e45da171e59eab0fafaf7363b123b88cbb6ded4
Marcella <marcellahastings@gmail.com>
2013-01-12 15:47:45 -0600

    Add name, balance to menu

--- src/com/example/android/beam/MenuActivity.java
+++ src/com/example/android/beam/MenuActivity.java
@@ -2,15 +2,23 @@ package com.example.android.beam;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MenuActivity extends Activity {
+	private SharedPreferences mPreferences;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView balance = (TextView) findViewById(R.id.balance);
+        name.setText("Hello " + mPreferences.getString("name", "Anonymous")+"!");
+        balance.setText("Balance: "+mPreferences.getInt("balance", 0));
         
         NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (mNfcAdapter == null) {
